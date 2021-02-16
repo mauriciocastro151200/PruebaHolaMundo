8d616880a462f355daa0cfb8b59f1c19ce68eaaa
Angel Mauricio Castro Cabrera <mauriciocastro.amcc@gmail.com>
2021-02-16 17:05:34 -0600

    Esta es mi primer versión

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+package holamundo;
+
+/**
+ *
+ * @author Mauricio Castro
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
